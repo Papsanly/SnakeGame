cedc688012520816d918ac798de5a5d1ee26cfbb
Papsanly <papsanlysenko@gmail.com>
2022-05-15 18:30:07 +0300

    add references to game objects in snake_game.py

--- settings.py
+++ settings.py
@@ -3,4 +3,8 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Settings:
-    resolution = (1000, 1000)
+    resolution = (15, 15)
+    grid_size = 75
+    snake_width = 0.625
+    food_radius = 0.5
+    speed = 1
--- snake_game.py
+++ snake_game.py
@@ -1,7 +1,9 @@
 import pygame
 
 from settings import Settings
-
+from snake import Snake
+from button import Button
+from food import Food
 
 class SnakeGame:
     """Main class to control game logic and manage objects"""
@@ -10,14 +12,34 @@ class SnakeGame:
         """Initiate pygame and create game objects"""
         pygame.init()
 
+        # start game active
+        self.is_active = True
+
+        # create game objects
+        self.foods = pygame.sprite.Group()
+        self.button = Button('Play')
+        self.snake = Snake()
+
         # create screen
         pygame.display.set_mode(Settings.resolution)
+        pygame.display.set_caption('Snake')
 
+    def _check_events(self):
+        """Check user input and other events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit(0)
+
+    def _update_screen(self):
+        """Update rendered objects"""
+        pygame.display.update()
 
     def run(self):
         """Run main game loop"""
         while True:
-            pygame.display.update()
+            self._check_events()
+            if self.is_active:
+                self._update_screen()
 
 
 if __name__ == '__main__':
