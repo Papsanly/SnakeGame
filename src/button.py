57b64bbfd044b269183c5a7f15835ffef94054f4
Andrew Lysenko <90246667+Papsanly@users.noreply.github.com>
2022-05-22 02:55:04 +0300

    Update button.py

--- src/button.py
+++ src/button.py
@@ -23,16 +23,12 @@ class Button:
             30
         )
 
-        # Start button unclicked
-        self.is_clicked = False
-
         # get button center for checking if clicked
         self.center = Settings.get_resolution() // 2 + Vector2(0, -0.5) * Settings.grid_size
 
     def check_clicked(self, mouse_pos):
         """Check if button is clicked to start animation"""
         if self.center.distance_to(mouse_pos) < Settings.grid_size:
-            self.is_clicked = True
             self.anim_data.set_timer()
 
     def draw(self):
