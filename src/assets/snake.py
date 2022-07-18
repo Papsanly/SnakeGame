480709ca97bccf402ddfe900baef6ae3df0e3f4b
papsan <papsanlysenko@gmail.com>
2022-07-18 13:33:57 -0700

    added snake body creation to snake constructor

--- src/assets/snake.py
+++ src/assets/snake.py
@@ -3,6 +3,7 @@ from typing import Sequence
 from pygame.sprite import Sprite, Group
 
 from src.assets.asset import AssetGroup
+from src.tile_position import TilePosition
 
 
 class Snake(AssetGroup):
@@ -11,3 +12,12 @@ class Snake(AssetGroup):
         super().__init__(*sprites)
 
         self.length = 1
+
+        self.add(
+            SnakeBody(position='center', direction='U', body_type='end'),
+            SnakeBody(position=TilePosition('center') + (0, 1), direction='D', body_type='end'),
+        )
+
+
+class SnakeBody(Sprite):
+    pass
---
+++ src/tile_position.py
@@ -0,0 +1,2 @@
+class TilePosition:
+    pass
