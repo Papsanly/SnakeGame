f9bb194e04a0efddaf567758a1e9e659ba707647
papsan <papsanlysenko@gmail.com>
2022-07-18 18:15:35 -0700

    created snake body class hierarchy

--- src/assets/snake.py
+++ src/assets/snake.py
@@ -12,14 +12,14 @@ BodyType = Literal['end', 'turn', 'straight']
 
 class Snake(AssetGroup):
 
-    def __init__(self, *sprites: Group):
-        super().__init__(*sprites)
+    def __init__(self, *groups: Group):
+        super().__init__(*groups)
 
         self.length = 1
 
         self.add(
-            SnakeBody('center', 'U', 'end', *sprites),
-            SnakeBody('center', 'D', 'end', *sprites, shift=(0, 1)),
+            SnakeBodyEnd('center', 'U', 'end', *groups),
+            SnakeBodyEnd('center', 'D', 'end', *groups, shift=(0, 1)),
         )
 
 
@@ -33,5 +33,38 @@ class SnakeBody(Sprite):
         self.direction = Orientation(direction)
         self.body_type = body_type
 
+
+class SnakeBodyEnd(SnakeBody):
+
+    def __init__(self, position: PositionStr, direction: DirectionStr | DirectionAngle,
+                 body_type: BodyType, *groups: Group, shift=(0, 0)):
+        super().__init__(position, direction, body_type, *groups, shift=shift)
+
         self.image = Utils.load_tile_image(f'../assets/snake_body/{body_type}_{self.direction}.bmp')
+        self.rect = self.image.get_rect(center=self.position.center)
+
+
+class SnakeBodyStraight(SnakeBody):
+
+    def __init__(self, position: PositionStr, direction: DirectionStr | DirectionAngle,
+                 body_type: BodyType, *groups: Group, shift=(0, 0)):
+        super().__init__(position, direction, body_type, *groups, shift=shift)
+
+        if direction in ('U', 'D', 90, 270):
+            direction = 'V'
+        elif direction in ('R', 'L', 0, 180):
+            direction = 'H'
+
+        self.image = Utils.load_tile_image(f'../assets/snake_body/{body_type}_{direction}.bmp')
+        self.rect = self.image.get_rect(center=self.position.center)
+
+
+class SnakeBodyTurn(SnakeBody):
+
+    def __init__(self, position: PositionStr, start_direction: DirectionStr | DirectionAngle,
+                 end_direction: DirectionStr | DirectionAngle,
+                 body_type: BodyType, *groups: Group, shift=(0, 0)):
+        super().__init__(position, end_direction, body_type, *groups, shift=shift)
+
+        self.image = Utils.load_tile_image(f'../assets/snake_body/{body_type}_{start_direction}{end_direction}.bmp')
         self.rect = self.image.get_rect(center=self.position.center)
